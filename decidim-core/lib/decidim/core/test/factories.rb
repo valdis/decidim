b1165eced0f38f73cd8f2e9a25e517e107e7cfd1
Agustí B.R <agusti.br@coditramuntana.com>
2018-07-24 16:58:43 +0200

    Refactor specs

--- decidim-core/lib/decidim/core/test/factories.rb
+++ decidim-core/lib/decidim/core/test/factories.rb
@@ -84,12 +84,13 @@ FactoryBot.define do
     official_url { Faker::Internet.url }
     highlighted_content_banner_enabled { false }
     enable_omnipresent_banner { false }
-    tos_version { Time.current }
     badges_enabled { true }
 
-    after(:create) do |organization|
-      tos_page = Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: organization)
-      create(:static_page, :tos, organization: organization) if tos_page.nil?
+    trait :with_tos do
+      after(:create) do |organization|
+        tos_page = Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: organization)
+        create(:static_page, :tos, organization: organization) if tos_page.nil?
+      end
     end
   end
 
@@ -108,8 +109,6 @@ FactoryBot.define do
     confirmation_sent_at { Time.current }
 
     after(:create) do |user|
-      tos_page = Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: user.organization)
-      create(:static_page, :tos, organization: user.organization) if tos_page.nil?
       user.accepted_tos_version = user.organization.tos_version
       user.save
     end
@@ -237,7 +236,7 @@ FactoryBot.define do
     organization
 
     trait :default do
-      slug { Decidim::StaticPage::DEFAULT_PAGES.sample }
+      slug { (Decidim::StaticPage::DEFAULT_PAGES - ["terms-and-conditions"]).sample }
     end
 
     trait :tos do
